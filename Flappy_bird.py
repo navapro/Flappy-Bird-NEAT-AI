67071eb7b919d866095662bae2110de81ae8b480
NK <45469562+navapro@users.noreply.github.com>
2020-07-27 16:23:05 -0600

    Move the base

--- Flappy_bird.py
+++ Flappy_bird.py
@@ -133,9 +133,18 @@ class Base:
         self.y = y
         self.x1 = 0
         self.x2 = WIDTH
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH  < 0:
+            self.x1 = self.x2 + self.WIDTH
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
    
 
-
+        
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
     bird.draw(win)
